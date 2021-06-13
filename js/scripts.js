5b9d4eab132205432f63262a64ca4acbe5273970
vrex808 <vrex808@email.com>
2021-06-13 12:19:30 -0700

    addded dark mode light mode to page

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $(".btn#navy").click(function() {
+    $("body").removeClass();
+    $("body").addClass("navy-background");
+});
+
+  $(".btn#white").click(function() {
+    $("body").removeClass(); 
+    $("body").addClass("white-background");
+  });
+});
+
+  
